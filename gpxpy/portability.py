c1a54d62087038ab5161d304046db9a1611f7464
Tomo Krajina <tkrajina@gmail.com>
2012-12-16 11:52:23 +0100

    removed has_key from portability

--- gpxpy/portability.py
+++ gpxpy/portability.py
@@ -24,17 +24,3 @@ except NameError:
     # Python 3
     def has_unicode_type():
         return False
-
-
-try:
-    # Python 2
-    trial_dict = dict()
-    trial_dict.has_key('foo')
-    def has_key(d, key):
-        return d.has_key(key)
-except AttributeError:
-    # Python 3
-    def has_key(d, key):
-        return key in d
-finally:
-    del trial_dict
