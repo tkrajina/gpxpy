80de8292ac2d60b7881875b40831f3400b5ef75d
Robert Smallshire <robert@smallshire.org.uk>
2012-12-22 19:49:22 +0100

    Simplify chained comparisons.

--- gpxpy/gpx.py
+++ gpxpy/gpx.py
@@ -781,9 +781,9 @@ class GPXTrackSegment:
         previous_point = None
         next_point = None
 
-        if 0 < point_no and point_no < len(self.points):
+        if 0 < point_no < len(self.points):
             previous_point = self.points[point_no - 1]
-        if 0 < point_no and point_no < len(self.points) - 1:
+        if 0 < point_no < len(self.points) - 1:
             next_point = self.points[point_no + 1]
 
         #mod_logging.debug('previous: %s' % previous_point)
