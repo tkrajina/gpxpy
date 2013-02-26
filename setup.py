4fcc29d9861ca6d522c87cd27c7129ff9db80f42
Tomo Krajina <tkrajina@gmail.com>
2013-02-26 06:57:38 +0100

    0.8.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import distutils.core as mod_distutilscore
 
 mod_distutilscore.setup(
     name = 'gpxpy',
-    version = '0.8.6',
+    version = '0.8.7',
     description = 'GPX file parser and GPS track manipulation library',
     license = 'Apache License, Version 2.0',
     author = 'Tomo Krajina',
