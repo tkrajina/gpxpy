6320cbd8dc26057bf825d676bd65bf255e7411f4
Tomo Krajina <tkrajina@gmail.com>
2013-07-07 06:22:50 +0200

    Default 'lxml' even if not available -- fixed

--- test.py
+++ test.py
@@ -126,6 +126,11 @@ class LxmlTests(mod_unittest.TestCase):
 
         return gpx
 
+    def test_parse_with_all_parser_typer(self):
+        self.assertTrue(mod_gpxpy.parse(open('test_files/cerknicko-jezero.gpx')))
+        self.assertTrue(mod_gpxpy.parse(open('test_files/cerknicko-jezero.gpx'), parser='minidom'))
+        self.assertTrue(mod_gpxpy.parse(open('test_files/cerknicko-jezero.gpx'), parser='lxml'))
+
     def test_simple_parse_function(self):
         # Must not throw any exception:
         mod_gpxpy.parse(open('test_files/korita-zbevnica.gpx'), parser=self.get_parser_type())
