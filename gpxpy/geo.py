eba5b27bd50e1bed2933bf7e439b1a2fad44fe63
Tomo Krajina <tkrajina@gmail.com>
2013-12-12 15:14:48 +0100

    Line class

--- gpxpy/geo.py
+++ gpxpy/geo.py
@@ -277,6 +277,65 @@ class Location:
         return '[loc:%s,%s@%s]' % (self.latitude, self.longitude, self.elevation)
 
     def __hash__(self):
-                return mod_utils.hash_object(self, 'latitude', 'longitude', 'elevation') 
+        return mod_utils.hash_object(self, 'latitude', 'longitude', 'elevation') 
 
+class Line:
+    """
+    Dummy line class.
+    """
+
+    def __init__(self, location1, location2):
+        self.location1 = location1
+        self.location2 = location2
+        self._init_line_equation()
+        self._normalize_abc()
+
+    def _init_line_equation(self):
+        """
+        Return a, b and c for the equation of type:
+
+            a * latitude + b * longitude + c = 0
+
+        Where sqrt(a**2 + b**2) = 1. That way the distance from the line can be computed by just insertint the latitude, and longitude into:
+
+            distance_from_line(latitude, longitude) a * latitude + b * longitude + c
+
+        And checking for shortening.
+        """
+        # First compute a, b for latitude = longitude * a + b
+        if self.location1.longitude == self.location2.longitude:
+            # Vertical line:
+            self.a = float(0)
+            self.b = float(1)
+            self.c = float(-self.location1.longitude)
+        else:
+            a = (self.location1.latitude - self.location2.latitude) / (self.location1.longitude - self.location2.longitude)
+            b = self.location1.latitude - self.location1.longitude * a
+            self.angle = mod_math.atan(a)
+            self.a = float(1)
+            self.b = float(-a)
+            self.c = float(-b)
+
+        if self.location1.latitude == self.location2.latitude:
+            self.angle = mod_math.pi / 2. 
+        else:
+            self.angle = mod_math.atan((self.location1.longitude - self.location2.longitude) / (self.location1.latitude - self.location2.latitude))
+
+        print 'angle=', self.angle
+    
+    def _normalize_abc(self):
+        normalization_coef = mod_math.sqrt(self.a**2 + self.b**2)
+
+        self.a = self.a / normalization_coef
+        self.b = self.b / normalization_coef
+        self.c = self.c / normalization_coef
+
+    def dummy_distance(self, location):
+        return abs(self.a * location.latitude + self.b * location.longitude + self.c)
+
+    def distance(self, location):
+        pass
+
+    def __str__(self):
+        return 'Dummy line %s*latitude + %s*longitude + %s = 0 (angle=%s)' % (self.a, self.b, self.c, self.angle)
 
