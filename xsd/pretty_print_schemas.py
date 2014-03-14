845c1bf21ee249fb8cdfa8cea2476fc4b2edfccd
Tomo Krajina <tkrajina@gmail.com>
2014-03-14 05:41:33 +0100

    1.0 schema fix

--- xsd/pretty_print_schemas.py
+++ xsd/pretty_print_schemas.py
@@ -1,5 +1,12 @@
 import xml.dom.minidom as mod_minidom
 
+def get_children_by_tag_name(node, tag_name):
+    result = []
+    for child in node.childNodes:
+        if child.nodeName == tag_name:
+            result.append(child)
+    return result
+
 def get_indented(indentation, string):
     result = ''
     for i in range(indentation):
@@ -22,16 +29,16 @@ def parse_1_1_element_node(gpx_element_node, complex_type_nodes, depth=0):
     result += get_indented(depth, '%s (%s)' % (node_name, node_type))
     if node_type in complex_type_nodes:
         # Complex node => parse:
-        attribute_nodes = complex_type_nodes[node_type].getElementsByTagName('xsd:attribute')
+        attribute_nodes = get_children_by_tag_name(complex_type_nodes[node_type], 'xsd:attribute')
         for attribute_node in attribute_nodes:
             attribute_node_name = attribute_node.attributes['name'].value
             attribute_node_type = attribute_node.attributes['type'].value
             attribute_node_required = attribute_node.attributes['required'].value if attribute_node.attributes.has_key('required') else None
-            result += get_indented(depth + 2, 'attr: %s (%s) %s' % (attribute_node_name, attribute_node_type, attribute_node_required))
-        sequence_nodes = complex_type_nodes[node_type].getElementsByTagName('xsd:sequence')
+            result += get_indented(depth + 1, '- attr: %s (%s) %s' % (attribute_node_name, attribute_node_type, attribute_node_required))
+        sequence_nodes = get_children_by_tag_name(complex_type_nodes[node_type], 'xsd:sequence')
         if len(sequence_nodes) > 0:
             sequence_node = sequence_nodes[0]
-            for element_node in sequence_node.getElementsByTagName('xsd:element'):
+            for element_node in get_children_by_tag_name(sequence_node, 'xsd:element'):
                 result += parse_1_1_element_node(element_node, complex_type_nodes, depth=depth+1)
     return result
 
@@ -58,15 +65,20 @@ def parse_1_0(dom):
 def parse_1_0_elements(element, depth=0):
     result = ''
     result += get_indented(depth, element.attributes['name'].value)
-    complex_types = element.getElementsByTagName('xsd:complexType')
+    complex_types = get_children_by_tag_name(element, 'xsd:complexType')
     if len(complex_types) > 0:
-        for sub_element in complex_types[0].getElementsByTagName('xsd:sequence')[0].getElementsByTagName('xsd:element'):
-            result += parse_1_0_elements(sub_element, depth=depth+1)
-    for attribute_node in element.getElementsByTagName('xsd:attribute'):
-        attribute_node_name = attribute_node.attributes['name'].value
-        attribute_node_type = attribute_node.attributes['type'].value
-        attribute_node_use = attribute_node.attributes['use'].value if attribute_node.attributes.has_key('use') else None
-        result += get_indented(depth + 2, 'attr: %s (%s) %s' % (attribute_node_name, attribute_node_type, attribute_node_use))
+        print(complex_types)
+        complex_type = complex_types[0]
+        for attribute_node in get_children_by_tag_name(complex_type, 'xsd:attribute'):
+            attribute_node_name = attribute_node.attributes['name'].value
+            attribute_node_type = attribute_node.attributes['type'].value
+            attribute_node_use = attribute_node.attributes['use'].value if attribute_node.attributes.has_key('use') else None
+            result += get_indented(depth + 1, '- attr: %s (%s) %s' % (attribute_node_name, attribute_node_type, attribute_node_use))
+        sequences = get_children_by_tag_name(complex_type, 'xsd:sequence')
+        if len(sequences) > 0:
+            sequence = sequences[0]
+            for sub_element in get_children_by_tag_name(sequence, 'xsd:element'):
+                result += parse_1_0_elements(sub_element, depth=depth+1)
     return result
 
 dom = mod_minidom.parseString(open('gpx1.0.xsd').read())
