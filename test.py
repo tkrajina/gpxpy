2dae99e2e28ad297b53f9dbdfe9ac1980f2b590d
Tomo Krajina <tkrajina@gmail.com>
2014-04-02 20:23:04 +0200

    More tests

--- test.py
+++ test.py
@@ -1677,6 +1677,8 @@ class AbstractTests:
                 self.assertEquals(gpx.bounds.min_latitude, 1.2)
                 self.assertEquals(get_dom_node(dom, 'gpx/bounds').attributes['minlat'].value, '1.2')
 
+                # Waypoints:
+
                 self.assertEquals(len(gpx.waypoints), 2)
 
                 self.assertEquals(gpx.waypoints[0].latitude, 12.3)
@@ -1997,6 +1999,66 @@ class AbstractTests:
                 self.assertEquals(get_dom_node(dom, 'gpx/metadata/extensions/aaa').firstChild.nodeValue, 'bbb')
                 self.assertEquals(get_dom_node(dom, 'gpx/metadata/extensions/bbb').firstChild.nodeValue, 'ccc')
                 self.assertEquals(get_dom_node(dom, 'gpx/metadata/extensions/ccc').firstChild.nodeValue, 'ddd')
+
+                self.assertEquals(2, len(gpx.waypoints))
+
+                self.assertEquals(gpx.waypoints[0].latitude, 12.3)
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]').attributes['lat'].value, '12.3')
+
+                self.assertEquals(gpx.waypoints[0].longitude, 45.6)
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]').attributes['lon'].value, '45.6')
+
+                self.assertEquals(gpx.waypoints[0].longitude, 45.6)
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]').attributes['lon'].value, '45.6')
+
+                self.assertEquals(gpx.waypoints[0].elevation, 75.1)
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/ele').firstChild.nodeValue, '75.1')
+
+                self.assertEquals(gpx.waypoints[0].time, mod_datetime.datetime(2013, 1, 2, 2, 3))
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/time').firstChild.nodeValue, '2013-01-02T02:03:00Z')
+
+                self.assertEquals(gpx.waypoints[0].magnetic_variation, 1.1)
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/magvar').firstChild.nodeValue, '1.1')
+
+                self.assertEquals(gpx.waypoints[0].geoid_height, 2.0)
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/geoidheight').firstChild.nodeValue, '2.0')
+
+                self.assertEquals(gpx.waypoints[0].name, 'example name')
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/name').firstChild.nodeValue, 'example name')
+
+                self.assertEquals(gpx.waypoints[0].comment, 'example cmt')
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/cmt').firstChild.nodeValue, 'example cmt')
+
+                self.assertEquals(gpx.waypoints[0].description, 'example desc')
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/desc').firstChild.nodeValue, 'example desc')
+
+                self.assertEquals(gpx.waypoints[0].source, 'example src')
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/src').firstChild.nodeValue, 'example src')
+
+                self.assertEquals(gpx.waypoints[0].link, 'http://link3')
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/link').attributes['href'].nodeValue, 'http://link3')
+
+                self.assertEquals(gpx.waypoints[0].link_text, 'link text3')
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/link/text').firstChild.nodeValue, 'link text3')
+
+                self.assertEquals(gpx.waypoints[0].link_type, 'link type3')
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/link/type').firstChild.nodeValue, 'link type3')
+
+                self.assertEquals(gpx.waypoints[0].url, 'example url')
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/url').firstChild.nodeValue, 'example url')
+
+                self.assertEquals(gpx.waypoints[0].url_name, 'example urlname')
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[0]/urlname').firstChild.nodeValue, 'example urlname')
+
+                self.assertEquals(gpx.waypoints[1].latitude, 13.4)
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[1]').attributes['lat'].value, '13.4')
+
+                self.assertEquals(gpx.waypoints[1].longitude, 46.7)
+                self.assertEquals(get_dom_node(dom, 'gpx/wpt[1]').attributes['lon'].value, '46.7')
+
+                self.assertEquals(2, len(gpx.waypoints[0].extensions))
+                self.assertEquals('bbb', gpx.waypoints[0].extensions['aaa'])
+                self.assertEquals('ddd', gpx.waypoints[0].extensions['ccc'])
 
     def test_default_values(self):
         obj = mod_gpx.GPXBounds()
