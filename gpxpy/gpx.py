c0770ebef7c6afd26c649a05e8c26e31d0087d80
pvbouwel <peter.vanbouwel@gmail.com>
2014-04-13 14:09:31 +0200

    Don't return None when speed between points is zero

--- gpxpy/gpx.py
+++ gpxpy/gpx.py
@@ -483,7 +483,7 @@ class GPXTrackPoint(mod_geo.Location):
         if not length:
             length = self.distance_2d(track_point)
 
-        if not seconds or not length:
+        if not seconds or length is not None:
             return None
 
         return length / float(seconds)
