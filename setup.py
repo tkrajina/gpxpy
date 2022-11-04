a108993a2ecc05b75ef95e1a71a053ee486fd7e2
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-11-04 19:59:17 +0200

    Add support for Python 3.9-3.11

--- setup.py
+++ setup.py
@@ -43,6 +43,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     scripts=['gpxinfo'],
 )
